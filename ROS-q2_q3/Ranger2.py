7555e5c412ce6c75baaf26f5f99bdcff25a27d9e
Muhi0709 <62468783+Muhi0709@users.noreply.github.com>
2020-03-21 18:55:07 +0530

    Update Ranger2.py

--- ROS-q2_q3/Ranger2.py
+++ ROS-q2_q3/Ranger2.py
@@ -3,17 +3,17 @@ from std_msgs.msg import String
 
 #the message/data gets printed directly
 def getmsg(stri):
-	print stri.data          
+    print stri.data          
 
 #subscriber gets the string message (of welcome_message topic)
 #the subscription ends only when the node/program quits(press ctrl+c to quit)	    
 def listen():
- 	r= rospy.Subscriber('welcome_message', String,getmsg)   
-	rospy.init_node('Ranger1', anonymous=True)
-	rospy.spin()                    
+    r= rospy.Subscriber('welcome_message', String,getmsg)   
+    rospy.init_node('Ranger1', anonymous=True)
+    rospy.spin()                    
 if __name__ == '__main__':
-	try:
-		listen()
-	except rospy.ROSInterruptException:
-		pass
+    try:
+        listen()
+    except rospy.ROSInterruptException:
+	pass
 
