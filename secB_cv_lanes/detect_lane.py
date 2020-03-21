6a63154588d49f56ce4433e76fcf09f469f381a4
Muhi0709 <62468783+Muhi0709@users.noreply.github.com>
2020-03-21 21:28:12 +0530

    Update detect_lane.py

--- secB_cv_lanes/detect_lane.py
+++ secB_cv_lanes/detect_lane.py
@@ -1,16 +1,23 @@
 import cv2 as cv
 import numpy as np
 
-original_image=cv.imread('lanes.jpeg')    
-gray_image=cv.cvtColor(original_image,cv.COLOR_RGB2GRAY)
-#to get  the edges in the image (containing the lanes)
-canny_image=cv.Canny(gray_image,200,220)                  
-det_lines=cv.HoughLinesP(canny_image,1,np.pi/180,50,minLineLength=100,maxLineGap=10)    
-for line in det_lines:
-    x1,y1,x2,y2=line[0]
-    #drawing lines over the detected lanes
-    cv.line(original_image,(x1,y1),(x2,y2),(0,0,255),3)   
+def lane_detect():
+    original_image=cv.imread('lanes.jpeg')    
+    gray_image=cv.cvtColor(original_image,cv.COLOR_BGR2GRAY)
+    #to get  the edges in the image (containing the lanes)
+    canny_image=cv.Canny(gray_image,210,220)                  
+    det_lines=cv.HoughLinesP(canny_image,1,np.pi/180,50,minLineLength=100,maxLineGap=10)    
+    for line in det_lines:
+        x1,y1,x2,y2=line[0]
+        #drawing lines over the detected lanes
+        cv.line(original_image,(x1,y1),(x2,y2),(0,0,255),3)   
     
-cv.imshow('detection',original_image)
-k=cv.waitKey(0)
-cv.destroyAllWindows()
+    cv.imshow('detection',original_image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+if __name__=="__main__":
+    try:
+        lane_detect()
+    except:
+        pass
